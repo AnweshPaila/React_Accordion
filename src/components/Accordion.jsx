6643977a051e61c9c977cf5b1aba6698e41b0d1a
AnweshPaila <anwesh1729@gmail.com>
2020-10-15 17:46:53 +0530

    updated hook naming conventions

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useRef } from "react";
-import Checkbox from "./Checkbox";
 
+import Checkbox from "./Checkbox";
 import Chevron from "./Chevron";
 
 import "./Accordion.css";
 
 function Accordion(props) {
-  const [setActive, setActiveState] = useState("");
-  const [setHeight, setHeightState] = useState("0px");
-  const [setRotate, setRotateState] = useState("accordion__icon");
+  const [active, setActive] = useState("");
+  const [height, setHeight] = useState("0px");
+  const [rotate, setRotate] = useState("accordion__icon");
   const [checkedItems, setCheckedItems] = useState({});
 
   const handleChange = (event) => {
@@ -16,31 +16,31 @@ function Accordion(props) {
       ...checkedItems,
       [event.target.name]: event.target.checked,
     });
-    console.log("checkedItems: ", checkedItems);
+    // console.log("checkedItems: ", checkedItems);
   };
 
   const content = useRef(null);
 
   function toggleAccordion() {
-    setActiveState(setActive === "" ? "active" : "");
-    setHeightState(
-      setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
+    setActive(active === "" ? "active" : "");
+    setHeight(
+      active === "active" ? "0px" : `${content.current.scrollHeight}px`
     );
-    setRotateState(
-      setActive === "active" ? "accordion__icon" : "accordion__icon rotate"
+    setRotate(
+      active === "active" ? "accordion__icon" : "accordion__icon rotate"
     );
   }
 
   return (
     <>
       <div className="accordion__section">
-        <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
+        <button className={`accordion ${active}`} onClick={toggleAccordion}>
           <p className="accordion__title">{props.title}</p>
-          <Chevron className={`${setRotate}`} width={15} fill={"#fff"} />
+          <Chevron className={`${rotate}`} width={15} fill={"#fff"} />
         </button>
         <div
           ref={content}
-          style={{ maxHeight: `${setHeight}` }}
+          style={{ maxHeight: `${height}` }}
           className="accordion__content"
         >
           <div className="accordion__text">
